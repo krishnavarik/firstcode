a7e6d5759579ee909236653c6b5d932b49209a1a
krishnavarik <krishnavarik9@gmail.com>
2021-11-23 16:55:52 +0530

    added dompart4

---
+++ dompart4.js
@@ -0,0 +1,48 @@
+
+
+var form = document.getElementById('addForm');
+var itemList =document.getElementById('items');
+
+//form submit Event----//
+
+form.addEventListener('submit', addItem);
+
+//-----aDdItem--//
+
+function addItem(e){
+    e.preventDefault();
+
+    //---------Get input Value-----//
+    var item = document.getElementById('item').value;
+    // console.log(item);
+
+    //-----create new Li element----//
+
+    var li = document.createElement('li');
+
+    //----class Name--//
+    li.className = 'list-group-item';
+    //---------Add text Node with Input Value-----//
+    
+    li.appendChild(document.createTextNode(item));
+
+    //------Create delete button-------//
+    
+    var deleteBtn = document.createElement('button');
+    
+    
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+    
+
+    //----------Append textNode---------//
+    deleteBtn.appendChild(document.createTextNode('x'));
+
+
+    //--------Append button to Li--//
+    li.appendChild(deleteBtn);
+
+    itemList.appendChild(li);
+
+
+
+}
